3ba75ca46eeef702a0e24367255f8f42a773cd5a
Antonio Vitalic <antoninte99@gmail.com>
2025-11-13 12:25:21 -0300

    fix: revert change in allowed hosts

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-me")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.13', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
